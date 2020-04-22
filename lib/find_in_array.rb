c214575e801f39f4c4beb3e717db15f7d59d596b
Diego Zegarra <diego.zegarra79@icloud.com>
2020-04-22 19:01:00 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,6 +2,8 @@ def find_element_index(array, value_to_find)
   counter = 0 
   while counter < array.length do
     if array[counter] == value_to_find do
-      
+    puts array[counter].index(value_to_find)
+  end
+  
     counter += 1 
 end
